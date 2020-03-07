8da7efd1c18cddc05934917fe42c3a6b5b1cdb73
Jose E Reyes <jreyes8631@gmail.com>
2020-03-07 23:49:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,6 @@
 
 def speak_to_grandma(phrase)
   answer "HUH?! SPEAK UP, SONNY!"
-  if phrase == "I LOVE YOU GRANDMA!"
-     return "NO, NOT SINCE 1938!"
-  else phrase == upcase.phrase
-     return == "I LOVE YOU TOO PUMPKIN!"
-  end
+ 
   
 end
